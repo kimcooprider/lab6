9cab4a5851e3b1797470c49f4036c96f188fd752
kimcooprider <kimcooprider@gmail.com>
2021-11-13 12:04:48 -0600

    added function

--- js/app.js
+++ js/app.js
@@ -6,11 +6,26 @@ let corgiNum = prompt('How many corgis do you own?');
 
 console.log('User has this number of corgis',corgiNum);
 
-let greeting;
 
-if(corgiNum > 0){
-greeting = 'Good job!  Corgis Rock!';
-} else{
-greeting = 'You should get a Corgi immediately.';
+function  getCorgiNum(){
+    let greeting;
+
+    if(corgiNum < 2 && corgiNum > 0){
+    greeting = 'Good job!  Corgis Rock!';
+    } else if(corgiNum === 3 || corgiNum < 5){
+    greeting = 'You are on your way!';
+    } else if (corgiNum >= 5){
+    greeting = 'Wow, that is a lot of corgis!';
+    }  else {  
+    greeting = 'You should get a Corgi immediately.';
+    }
+    document.write(greeting);
+
+
+
+
+console.log('proof of life');
 }
-document.write(greeting);
+
+
+getCorgiNum();
